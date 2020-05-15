a900830f216b84cba671b509f9cd31aced6da77b
HUNG CHU <cphung1997@gmail.com>
2020-05-15 17:23:13 -0500

    Add the application of @Controller and Thymeleaf

---
+++ ControllerAndThymeleaf/src/main/java/com/example/hungpchu/ControllerAndThymeleaf/WebController/WebController.java
@@ -0,0 +1,29 @@
+package com.example.hungpchu.ControllerAndThymeleaf.WebController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about()
+    {
+        return "about";
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name" , required = false, defaultValue = "Hung") String name, Model model)
+    {
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
