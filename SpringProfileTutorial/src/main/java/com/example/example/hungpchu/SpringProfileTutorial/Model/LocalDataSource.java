722f614c31f72a02044dce60193585102a258652
HUNG CHU <cphung1997@gmail.com>
2020-05-20 17:57:45 -0500

    Add the explanation about the @Autowired

--- SpringProfileTutorial/src/main/java/com/example/example/hungpchu/SpringProfileTutorial/Model/LocalDataSource.java
+++ SpringProfileTutorial/src/main/java/com/example/example/hungpchu/SpringProfileTutorial/Model/LocalDataSource.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Value;
  * Use @Component is another way to create component or bean in the applicationContext
  * -> Use @Value{value} to init the value of the property in the class
  *
+ * @Autowired used only when the object is having bean or component in the
+ * application context
+ *
  * In this application, I will use the 2nd Step with @Configuration and @Bean
  */
 
